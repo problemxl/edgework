0aeb755b45f374a878f366f3fe911575d9156aa1
Mark <email@email.com>
2024-04-15 01:04:38 -0400

    Add SkaterStats and Shift models to the repository

--- edgework/models/shift.py
+++ edgework/models/shift.py
@@ -22,14 +22,37 @@ class PeriodTime:
 
 @dataclass
 class Shift:
+    """
+    Shift dataclass to store shift information.
+
+    A shift is a period of time where a player is on the ice.
+    """
+
+    """Shift ID is a unique identifier for the shift."""
     shift_id: int
+
+    """Game ID is the unique identifier for the game the shift is in."""
     game_id: int
+
+    """Player ID is the unique identifier for the player on the shift."""
     player_id: int
+
+    """Player name is the name of the player on the shift."""
     player_name: str
+
+    """Period is the period of the game the shift is in."""
     period: int
+
+    """Shift start is the time the shift started."""
     shift_start: PeriodTime
+
+    """Shift end is the time the shift ended."""
     shift_end: PeriodTime
+
+    """Duration is the length of the shift."""
     duration: timedelta
+
+    """Shift number is the order of the shift in the game."""
     shift_number: int
 
     def __init__(
@@ -41,6 +64,7 @@ class Shift:
         period: int,
         shift_start: str,
         shift_end: str,
+        shift_number: int,
     ):
         self.shift_id = shift_id
         self.game_id = game_id
@@ -50,6 +74,7 @@ class Shift:
         self.shift_start = PeriodTime(shift_start)
         self.shift_end = PeriodTime(shift_end)
         self.duration = timedelta(seconds=self.shift_end.total_seconds - self.shift_start.total_seconds)
+        self.shift_number = shift_number
 
     @property
     def shift_length(self):
