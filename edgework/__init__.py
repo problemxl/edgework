8ee5c0f7c9bba275fd4d5f650894ab812b7d3bf7
Mark <mark.franciscus94@gmail.com>
2026-01-24 00:45:01 -0500

    chore: bump version to 0.4.2

--- edgework/__init__.py
+++ edgework/__init__.py
@@ -1,7 +1,11 @@
-"""Edgework NHL API Client - Version 0.4.0"""
+"""Edgework NHL API Client - Version 0.4.2"""
 
-__version__ = "0.4.0"
+__version__ = "0.4.2"
 
-from .edgework import Edgework
+import sys
+
+sys.path.insert(0, "/home/mark/edgework")
+
+from edgework.edgework import Edgework
 
 __all__ = ["Edgework", "__version__"]
